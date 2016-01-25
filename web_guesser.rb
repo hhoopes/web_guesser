e07408d974fb991d6dc3ec27697d06d42352ac70
hhoopes <heidih@gmail.com>
2016-01-25 16:48:56 -0700

    Add color settings and number of guesses.

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,33 +2,61 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'pry'
 
-SECRET_NUMBER = rand(100)
+@@number = rand(100)
+@@guesses = 0
+
+get '/' do
+    guess = params["guess"]
+    color = get_color(guess) || "#FFFFFF"
+    message = check_guess(guess)
+  erb :index, :locals => {:number => @@number, message: message, color: color}
+end
+
+def reset_game
+  @@guesses = 0
+  @@number = rand(100)
+end
 
 def check_guess(guess)
   unless guess.nil?
-  message = ""
-  diff = (guess - SECRET_NUMBER).abs
-  if guess > SECRET_NUMBER && diff > 5
-    "Way too high!"
-  elsif guess > SECRET_NUMBER
-    "Too high!\n The SECRET NUMBER is #{SECRET_NUMBER}"
-  elsif guess == SECRET_NUMBER
-    "You got it right!"
-  elsif guess < SECRET_NUMBER && diff > 5
-    "Way too low!"
-  elsif guess < SECRET_NUMBER
-    "Too low!"
-  else
-    ""
+    message = ""
+    @@guesses += 1
+    guess = guess.to_i
+    if guess != @@number
+      if guess > @@number + 5
+        message << "Way too high!"
+      elsif guess > @@number
+        message << "Too high!"
+      elsif guess < @@number - 5
+        message << "Way too low!"
+      elsif guess < @@number
+        message << "Too low!"
+      else
+        message << "What is your guess?"
+      end
+      message << "\nYou've used #{@@guesses} guesses."
+      if @@guesses >= 5
+        message << "You've used too many guesses. Let's start over."
+        reset_game
+      end
+    elsif guess == @@number
+    message << "You got it right! The SECRET NUMBER is #{@@number}\n. It took you #{@@guesses} guesses. Let's try again."
+    reset_game
+    end
   end
+  message
 end
 
-end
-get '/' do
-  guess = params["guess"].to_i unless params["guess"].nil?
-  message = check_guess(guess)
-
-  erb :index, :locals => {:number => SECRET_NUMBER, message: message}
-
-
+def get_color(guess)
+  return "FFFFFF" unless guess
+  guess = guess.to_i
+  if (guess > @@number + 5) || (guess < @@number - 5)
+    "#CC0000"
+  elsif (guess > @@number) || (guess < @@number)
+    "#FFCCFF"
+  elsif guess == @@number
+    "#00CC00"
+  else
+    "#FFFFFF"
+  end
 end
